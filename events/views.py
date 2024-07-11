538e105ba040e2c1194a39b4a7e056ed22a0a620
Kiniboua <olivierkiniboua99@gmail.com>
2024-07-11 12:47:06 +0100

    Determine Correct User to edit event and add Venue Owner

--- events/views.py
+++ events/views.py
@@ -199,7 +199,10 @@ def add_venue(request):
     if request.method == "POST":
         form = VenueForm(request.POST)
         if form.is_valid():
-            form.save()
+            venue = form.save(commit=False)
+            venue.owner = request.user.id #Logged in User
+            venue.save()
+            #form.save()
             return HttpResponseRedirect('/add_venue?submitted=True')
     else:
         form = VenueForm
